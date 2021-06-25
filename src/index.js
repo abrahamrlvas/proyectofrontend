2cdf96ccf257f1c5303f9690ecc04f76a32d6071
Abraham Rivas <workabrahamrlvas@gmail.com>
2021-06-25 15:32:15 -0400

    Estructura de carpetas e instalados modulos para el proyecto. Conexion con la base de datos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+const database = require('./settings/databaseConnection');
 
-app.listen(3000, () => {
-  console.log('Server on port', 3000);
-})
+dotenv.config()
+const app = express();
+const port = process.env.PORT || 4000
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:8080' || 'http://localhost:8081'],
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  credentials: true
+}));
+
+app.listen(port, async () => {
+  try {
+    await database.sync();
+    console.log('Server listening in port', port);
+    console.log('Database is connected succesfully');
+  } catch (error) {
+    console.error('Something is wrong', error);
+  }
+});
