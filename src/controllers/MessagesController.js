61122fc099243b6a263483a756b95d73f75114b8
Abraham <workabrahamrlvas@gmail.com>
2021-07-21 12:39:09 +0800

    Agregada ruta para Obtener el ultimo mensaje de la conversacion

--- src/controllers/MessagesController.js
+++ src/controllers/MessagesController.js
@@ -11,28 +11,43 @@ class MessageController {
       ],
       order: [["createdAt", "ASC"]],
     });
-    res
-      .status(200)
-      .json(messages);
+    res.status(200).json(messages);
   }
 
   async getMessagePrivate(req, res) {
-    const { receiver, sender } = req.body
+    const { receiver, sender } = req.body;
     try {
       const messages = await Message.findAll({
         where: {
           receiver: {
-            [Op.or]: [receiver, sender]
+            [Op.or]: [receiver, sender],
           },
           sender: {
-            [Op.or]: [sender, receiver]
-          }
-        }
-      })
-      res.json(messages)
-    } catch (error) {  
+            [Op.or]: [sender, receiver],
+          },
+        },
+      });
+      res.json(messages);
+    } catch (error) {
       console.log(error);
     }
+  }
+
+  async getFindMessage(req, res) {
+    const { receiver, sender } = req.body
+    const message = await Message.findOne({
+      limit: 1,
+      where: {
+        receiver: {
+          [Op.or]: [receiver, sender],
+        },
+        sender: {
+          [Op.or]: [sender, receiver],
+        },
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.json(message)
   }
 }
 
--- src/routes/messages.routes.js
+++ src/routes/messages.routes.js
@@ -5,6 +5,7 @@ const conversationController = require("../controllers/conversationController");
 
 router.get("/messages", messageController.getMessage);
 router.post("/message", messageController.getMessagePrivate);
+router.post("/message/time", messageController.getFindMessage);
 router.post("/conversation", conversationController.getConversation);
 
 module.exports = router;
